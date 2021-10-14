c8163f3ce71719c97142dc2725b3e0d768070766
Pema Lama <pema.srp72@gmail.com>
2021-10-13 22:43:35 -0400

    commit tree 57d2a35b1e570e9a504a79435b49954c4503366f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arr){
+ return arr.slice(0,2); // return [arr[0],arr[1]];
+}
+function returnLastTwoDrivers(arr){
+    return arr.slice(-2);
+}
+
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(n){
+return function(n2){
+   return n * n2;
+}
+}
+const fareDoubler= createFareMultiplier(2);
+const fareTripler = function(n){
+  return createFareMultiplier(3)
+}
+
+// function  fareTripler(n){
+//    let tripleFare = createFareMultiplier() * 3;
+//  return tripleFare;
+// }
+
+function selectDifferentDrivers(arr, func){
+   return func(arr);
+}
